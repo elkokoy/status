3a79e493180bce7bf6423a88a5c2383acea0677d
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-16 07:35:29 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,57 @@
+const Eris = require("eris");
+var bot = new Eris("NTIzNjQ0MjMxOTY1NzM3MDAw.Dvchgg.TzSSHODNsRXp9RVuzXCaquIlz20");
+//** لاتعدل على شي تحت ذا **//
+bot.on("ready", () => {
+onstart();
+});
+
+function onstart(){
+
+setInterval(function(){
+
+var currentTime = new Date(),
+hours = currentTime.getHours() + 0 ,
+minutes = currentTime.getMinutes(),
+seconds = currentTime.getSeconds();
+
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+bot.editChannel("523645440265027585", { name : "●⌠ " + "users : " + bot.users.size + " ⌡●"});
+bot.editChannel("523645402382073867", { name : "●⌠ " + "servers : " + bot.guilds.size + " ⌡●"});
+bot.editChannel("523645471248482324", { name : "✲"});
+bot.editChannel("523645471248482324", { name : "✲W"});
+bot.editChannel("523645471248482324", { name : "✲We"});
+bot.editChannel("523645471248482324", { name : "✲Wel"});
+bot.editChannel("523645471248482324", { name : "✲Welc"});
+bot.editChannel("523645471248482324", { name : "✲Welco"});
+bot.editChannel("523645471248482324", { name : "✲Welcom"});
+bot.editChannel("523645471248482324", { name : "✲Welcome"});
+bot.editChannel("523645471248482324", { name : "✲Welcome "});
+bot.editChannel("523645471248482324", { name : "✲Welcome T"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To "});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊ"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯ"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪𝒴"});
+bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪𝒴✲"});
+
+
+
+
+
+}, 4000);
+
+}
+
+bot.connect();
