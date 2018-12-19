f21ac99bccdc28757bc9161ca2a43c92d0a306e3
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-19 06:01:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,7 @@ const Eris = require("eris");
 var bot = new Eris(process.env.BOT_TOKEN);
 //** لاتعدل على شي تحت ذا **//
 bot.on("ready", () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by Ahmed');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
-client.user.setStatus("dnd");
-});
-client.on('ready', () => {
+
      client.user.setActivity("!!help | للاستعمال",{type: 'WATCHING'})
   
 onstart();
